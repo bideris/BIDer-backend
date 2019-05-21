34383360202fc8cc221e40b33affbacd7475a0d9
BenasTB <budnikasb@gmail.com>
2019-05-21 15:24:50 +0300

    fixing fixes

--- src/main/java/com/bideris/dbservice/resource/TimerServiceResource.java
+++ src/main/java/com/bideris/dbservice/resource/TimerServiceResource.java
@@ -48,7 +48,7 @@ public class TimerServiceResource {
             int time = (int)( (a.getStartDate().getTime() -now.getTime()  ) / (1000 * 60 * 60 * 24));
             log.info("IKI STARTO LIKO {} ",time);
             log.info("iki aukciojo {}  \n pradzios liko {}",a.getStartDate(), time);
-            if(time < 1 && !a.getStatus().equals("Ended")){
+            if(time < 0 && !a.getStatus().equals("Ended")){
                 log.info("Pakeite statusa");
                 a.setStatus("Started");
                 auctionRepository.save(a);
@@ -82,7 +82,7 @@ public class TimerServiceResource {
             int time = (int)( (a.getStartDate().getTime() -now.getTime()  )  / (1000 * 60 * 60 * 24));
             log.info("iki aukciono {} \n pabaigos liko {}",a.getStartDate(), time + a.getDuration());
 
-            if (time + a.getDuration() < 1) {
+            if (time + a.getDuration() < 0) {
 
                 if(Setwinner(a)) {
                     log.info("STATUSA PAKEITE I ENDED IR PRIDEJO LAIMETOJA");
@@ -113,7 +113,7 @@ public class TimerServiceResource {
         log.info(now.toString());
         for (Auction a: auctions) {
 
-            int time = (int)( ( a.getStartDate().getTime() -now.getTime()  )  / (1000 * 60 * 60 * 24));
+            int time = (int)( ( a.getStartDate().getTime() -now.getTime()  )  / (1000 * 60 * 60 * 24)) -1;
             log.info("iki aukciojo {} \n pradzios  liko {}", a.getStartDate() ,time );
 
             if(time < 10){
